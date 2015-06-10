6f6804b338233a532081510a5c0f9542ac0b23d6
HubertHuang <hyx0408@126.com>
2015-06-10 15:40:50 +0800

    Initial commit

---
+++ src/examples/ConcurrPrint.java
@@ -0,0 +1,17 @@
+package examples;
+
+/**
+ * Created by hubert on 2015.06.10.
+ */
+public class ConcurrPrint {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread("Thread"+i){
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName());
+                }
+            }.start();
+        }
+    }
+}
