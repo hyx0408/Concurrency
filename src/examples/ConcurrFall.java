74a6aa77811921f81307c2f1a0d0ab6a7a451beb
HubertHuang <hyx0408@126.com>
2015-06-11 10:31:33 +0800

    using sout in Count.add(int i)

--- src/examples/ConcurrFall.java
+++ src/examples/ConcurrFall.java
@@ -5,15 +5,8 @@ package examples;
  */
 public class ConcurrFall {
     public static void main(String[] args) {
-        class Counter {
-            public int sum = 0;
-            //should use synchronized to correct
-            public int add(int i) {
-                sum += i;//If you want something wrong, you should make this process more complicated.
-                return sum;
-            }
-        }
         Counter myCounter = new Counter();
+        Integer myInt = 0;
         class MyThread extends Thread {
             int i = 0;
 
@@ -24,20 +17,28 @@ public class ConcurrFall {
 
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName() + " executed." );
-                myCounter.add(this.i);
+                myCounter.add(i);
             }
         }
         MyThread[] myThreads = new MyThread[10];
-        for (int i = 0; i < 10; i++) {
-            myThreads[i] = new MyThread("Thread" + i, i);
+        for (; myInt < 10; myInt++) {
+            myThreads[myInt] = new MyThread("Thread" + myInt, myInt);
         }
         //wrong way show the sum
         for (int i = 0; i < 10; i++) {
             myThreads[i].start();
-            System.out.println("Sum is "+ myCounter.sum+".");
         }
+    }
+}
 
+class Counter {
+    public int sum = 0;
 
+    //should use synchronized to correct
+    public int add(int i) {
+        sum += i;//If you want something wrong, you should make this process more complicated.
+        System.out.println(Thread.currentThread().getName() + " executed.");
+        System.out.println("Sum is "+this.sum+".");
+        return sum;
     }
 }
