42fd497533ed3eb0dd10752d1cd34e8e22d50f99
HubertHuang <hyx0408@126.com>
2015-06-15 14:09:44 +0800

    Fined Blocking Queue

--- src/examples/BlockingQueue.java
+++ src/examples/BlockingQueue.java
@@ -1,24 +1,26 @@
 package examples;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * Created by hubert on 2015.06.14.
  */
 public class BlockingQueue {
+    static class Producer implements Runnable {
+        java.util.concurrent.BlockingQueue<String> ldq = null;
 
-    public static void main(String[] args) {
-        class Producer implements Runnable {
-            LinkedBlockingQueue<String> ldq = null;
+        Producer(java.util.concurrent.BlockingQueue<String> ldq) {
+            this.ldq = ldq;
+        }
 
-            Producer(LinkedBlockingQueue<String> ldq) {
-                this.ldq = ldq;
-            }
-
-            @Override
-            public void run() {
-                Integer i = (int) (100 * Math.random());
-                String s = i.toString();
+        @Override
+        public void run() {
+            Integer i = (int) (100 * Math.random());
+            String s = i.toString();
+            synchronized (ldq) {
                 if (ldq.offer(s)) {
                     System.out.println(Thread.currentThread().getName() + " offers " + s +
                             " and now queue have " + ldq.size() + " numbers.");
@@ -27,25 +29,33 @@ public class BlockingQueue {
                 }
             }
         }
-        class Consumer implements Runnable {
-            LinkedBlockingQueue<String> ldq = null;
+    }
 
-            Consumer(LinkedBlockingQueue<String> ldq) {
-                this.ldq = ldq;
-            }
+    static class Consumer implements Runnable {
+        java.util.concurrent.BlockingQueue<String> ldq = null;
 
-            @Override
-            public void run() {
-                String s = ldq.poll();
+        Consumer(java.util.concurrent.BlockingQueue<String> ldq) {
+            this.ldq = ldq;
+        }
+
+        @Override
+        public void run() {
+            String s = ldq.poll();
+            synchronized (ldq) {
                 if (s != null) {
-                    System.out.println(Thread.currentThread().getName() + " gets " + s + " and now queue have" +
+                    System.out.println(Thread.currentThread().getName() + " gets " + s + " and now queue have " +
                             ldq.size() + " numbers.");
                 } else {
                     System.out.println(Thread.currentThread().getName() + " ----Queue is empty.");
                 }
             }
         }
-        LinkedBlockingQueue<String> myLBQ = new LinkedBlockingQueue<>(3);
+    }
+
+    public static void main(String[] args) {
+        java.util.concurrent.BlockingQueue<String> myLBQ = new PriorityBlockingQueue<>(3);
+        //new ArrayBlockingQueue<>(4);
+        //new LinkedBlockingQueue<>(4);
         Producer producer = new Producer(myLBQ);
         Consumer consumer = new Consumer(myLBQ);
         Thread[] myProducers = new Thread[6];
